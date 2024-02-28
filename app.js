21d72db3eab5f8e45e218c5f596c3e18678b72d1
NicolasLerch <lerchnicolas@hotmail.com>
2024-02-27 22:17:04 -0300

    listo para deploy

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ app.use(express.json());
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
-
-app.listen(3000, () => {
-    console.log('Servidor corriendo en el puerto 3000.')
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Servidor corriendo en el puerto ${port}.`)
 });
 
 app.get('/', (req, res) => {
